101e48013ec773d1a4dfbb89562d5bda9f059392
milkpanth <milkpanth@yahoo.com>
2020-03-20 18:46:07 +0700

    deleteTask

--- src/App.js
+++ src/App.js
@@ -3,14 +3,7 @@ import React, { useState,useEffect  }from 'react';
 import './App.css';
 import {firestore} from './index'
 function App() {
-  const [tasks,setTasks] = useState([
-    { 
-       id:1, name: "do homework"
-     },
-     {
-        id:2, name: "write node"
-     }
-   ])
+  const [tasks,setTasks] = useState([])
    const [ name,setName ] = useState([
 
   ])
@@ -28,18 +21,24 @@ function App() {
       setTasks(myTask)
     } )
   }
+  const deleteTask = (id) => {
+    firestore.collection("tasks").doc(id+'').delete()
+  }
   const renderTask = () => {
     if (tasks && tasks.length)
       return tasks.map((task,index)=>{
           return(
-            <li key={index}> {task.id} : {task.name}</li>
+            <li key={index}> 
+             {task.id} : {task.name}
+             <button onClick={() => deleteTask(task.id)}>Delete</button>
+             </li>
           )
         })
     else
          return (<li>No task</li>)
    }
    const addTask = () => {
-    let id = tasks[tasks.length-1].id+1
+    let id = ( tasks.length ===0)?1:tasks[tasks.length-1].id+1
     firestore.collection("tasks").doc(id+'').set({id,name})
   }
   return (
