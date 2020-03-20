a6f009f9a273a82dae017682c187a184aa6c6e83
milkpanth <milkpanth@yahoo.com>
2020-03-20 18:01:14 +0700

    useState array

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import React, { useState }from 'react';
+import React, { useState,useEffect  }from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   const [tasks,setTasks] = useState([
     { 
-      id:1, name: "do homework"
-    },
-    {
-    id:2, name: "write node"
-    }
-  ])
+       id:1, name: "do homework"
+     },
+     {
+        id:2, name: "write node"
+     }
+   ])
+  useEffect( () => {
+
+  })
   const renderTask = () => {
-    return tasks.map((task,index)=>{
-        return(
-          <li key={index}> {task.id} : {task.name}</li>
-        )
-      })
+    if (tasks && tasks.length)
+       return tasks.map((task,index)=>{
+           return(
+             <li key={index}> {task.id} : {task.name}</li>
+           )
+         })
+     else
+         return (<li>No task</li>)
   }
   return (
     <div >
