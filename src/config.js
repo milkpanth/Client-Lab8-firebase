9f6d2fadeac19c8ae9151b5eddc849208fde9347
milkpanth <milkpanth@yahoo.com>
2020-03-20 18:05:23 +0700

    add config

---
+++ src/config.js
@@ -0,0 +1,11 @@
+const config = {
+    apiKey: "AIzaSyDYsJtyDXXALUv22L9Pavkfd2jE9xtlWJA",
+    authDomain: "todo-19ae5.firebaseapp.com",
+    databaseURL: "https://todo-19ae5.firebaseio.com",
+    projectId: "todo-19ae5",
+    storageBucket: "todo-19ae5.appspot.com",
+    messagingSenderId: "212236470324",
+    appId: "1:212236470324:web:7c676305f1144458b8a261",
+    measurementId: "G-B5ZPX0MFMJ"
+  };
+export default config;
