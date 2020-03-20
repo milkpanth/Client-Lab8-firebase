d67ddfaaa7c21925f8f9f99bbb8ff0d563a74aba
milkpanth <milkpanth@yahoo.com>
2020-03-20 17:52:46 +0700

    renderTask

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState }from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [tasks,setTasks] = useState([
+    { 
+      id:1, name: "do homework"
+    },
+    {
+    id:2, name: "write node"
+    }
+  ])
+  const renderTask = () => {
+    return tasks.map((task,index)=>{
+        return(
+          <li key={index}> {task.id} : {task.name}</li>
+        )
+      })
+  }
   return (
     <div >
-      fghfghfgth
+      <h1>Todo</h1>
+         <ul>{ renderTask() }</ul>
     </div>
   );
 }
